9b696b89a3637342a5726a14b8271287e9f97301
gonsanmar2 <gonsanmar2@alum.us.es>
2024-03-04 15:18:34 +0100

    fix: lista array models

--- banquetBuddy/core/models.py
+++ banquetBuddy/core/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User, AbstractUser
 from enum import Enum
 from phonenumber_field.modelfields import PhoneNumberField
@@ -114,8 +113,11 @@ class Menu(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    plates = ArrayField(models.CharField(max_length=255), blank=True, null=True)
+    plates = models.ManyToManyField('Plate', related_name='menus', null=True, blank=True)
     diet_restrictions = models.CharField(max_length=255)
+
+class Plate(models.Model):
+    name = models.CharField(max_length=255)
 
 class Review(models.Model):
     particular = models.ForeignKey(Particular, on_delete=models.CASCADE, related_name='reviews')
