5bc0d6cc97ae9e0bc0491d7509691a01d5219d02
gonsanmar2 <gonsanmar2@alum.us.es>
2024-03-09 12:01:30 +0100

    feat: Busqueda caterings
    Hecho buscador por nombre de los caterings

--- banquetBuddy/caterings/views.py
+++ banquetBuddy/caterings/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 from core.models import CateringService
+from django.db.models import Q
 
 
 def listar_caterings(request):
+    context = {}
     caterings = CateringService.objects.all()
-    context = {'caterings': caterings}
+    if 'buscar' not in context:
+        busqueda = ''
+
+    if request.method == 'POST':
+        busqueda = request.POST.get('buscar', '')
+        caterings = CateringService.objects.filter(Q(name__icontains=busqueda))
+        print(caterings)
+        
+    context['buscar'] = busqueda    
+    context['caterings'] = caterings
     return render(request, 'listar_caterings.html', context)
