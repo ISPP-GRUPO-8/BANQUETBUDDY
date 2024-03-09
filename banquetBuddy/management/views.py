e19d13613c574fb3961ac6505baeada823a53935
marsannar2 <soymarcosr@gmail.com>
2024-03-09 15:31:20 +0100

    fix:user permission required

--- banquetBuddy/management/views.py
+++ banquetBuddy/management/views.py
@@ -21,17 +21,21 @@ def confirm_reservation(request, catering_service_id, event_id):
 @login_required
 def view_reservations(request, catering_service_id):
 
-    #catering_service = get_object_or_404(CateringService, pk=catering_service_id)
-    #if request.user == catering_service.cateringcompany.user:  
+    catering_service = get_object_or_404(CateringService, pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:  
         catering_service = get_object_or_404(CateringService, pk=catering_service_id)
         reservations = catering_service.events.all()
         return render(request,'reservations.html',{'reservations': reservations,'catering_service':catering_service})
-    #else:
+    else:
         return HttpResponseForbidden("You don't have permission to view this reservations.")
     
 @login_required
 def view_reservation(request, event_id,catering_service_id):
-    event = get_object_or_404(Event, pk=event_id)
-    return render(request,'view_reservation.html',{'event': event})
+    catering_service = get_object_or_404(CateringService, pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:  
+        event = get_object_or_404(Event, pk=event_id)
+        return render(request,'view_reservation.html',{'event': event})
+    else:
+        return HttpResponseForbidden("You don't have permission to view this reservation.")
 
 
