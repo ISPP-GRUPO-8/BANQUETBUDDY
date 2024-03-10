09d8112aa2215f09c5b081460ad2cca645346b69
edubusluc <edubusluc@alum.us.es>
2024-03-10 13:57:24 +0100

    feat: Add review functionality

--- banquetBuddy/caterings/urls.py
+++ banquetBuddy/caterings/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', listar_caterings, name='listar_caterings'),
     path('<int:catering_id>/', catering_detail, name='catering_detail'), 
+    path('<int:catering_id>/review', catering_review, name='add_review'), 
 
 ]
--- banquetBuddy/caterings/views.py
+++ banquetBuddy/caterings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-
-from core.models import CateringService
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from datetime import datetime
+from core.models import CateringService, Particular, Review
 
 
 def listar_caterings(request):
@@ -13,3 +14,29 @@ def catering_detail(request, catering_id):
     catering = get_object_or_404(CateringService, id = catering_id)
     context['catering'] = catering
     return render(request, 'catering_detail.html', context)
+
+def catering_review(request, catering_id):
+    catering = get_object_or_404(CateringService, id=catering_id)
+    user = request.user
+    particular = get_object_or_404(Particular, user_id=user.id)
+    context = {'catering': catering}
+
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        rating = request.POST.get('rating')
+
+        review = Review.objects.create(
+            cateringservice=catering,
+            particular=particular,
+            date=datetime.now().date(),
+            description=description,
+            rating=rating
+        )
+
+        url_catering = reverse('catering_detail', args=[catering.id])
+        return redirect(url_catering)
+
+    return render(request, 'catering_review.html', context)
+
+
+
