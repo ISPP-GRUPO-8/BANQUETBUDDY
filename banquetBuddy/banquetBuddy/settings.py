c29dd42da310eb21dbbd4b03973638afe636f6cd
gonsanmar2 <gonsanmar2@alum.us.es>
2024-03-12 08:52:26 +0100

    fix: Busqueda caterings
    Borrado duplicado en settings debido a cambio en modulos

--- banquetBuddy/banquetBuddy/settings.py
+++ banquetBuddy/banquetBuddy/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'catering_particular',
     'phonenumber_field',
     'catering_owners',
     'catering_particular',
