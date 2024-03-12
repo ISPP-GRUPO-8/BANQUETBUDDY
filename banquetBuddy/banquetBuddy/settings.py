c6dd10e5270ad26aa843bc39f681263f9db30c1c
elguille2 <guillermo2pacheco@gmail.com>
2024-03-12 13:02:32 +0100

    feat: Adds possibility to view verification document

--- banquetBuddy/banquetBuddy/settings.py
+++ banquetBuddy/banquetBuddy/settings.py
@@ -140,6 +140,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/images/logos'),
 ]
 
+# Configuración para manejar archivos de medios
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- banquetBuddy/catering_owners/urls.py
+++ banquetBuddy/catering_owners/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,5 +8,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', home, name='home'),
-    path('register_company',views.register_company,name='register_company'),
+    path('register_company', views.register_company, name='register_company'),
 ]
+
+# Configuración para servir archivos estáticos y de medios durante el desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
