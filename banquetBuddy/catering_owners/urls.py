519bf062645d82c976e6a7e653832f2d9e1b4ab4
marsannar2 <soymarcosr@gmail.com>
2024-03-14 18:24:54 +0100

    feature:added booking calendar for companies

--- banquetBuddy/catering_owners/urls.py
+++ banquetBuddy/catering_owners/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('applicants/<int:offer_id>/', employee_applications, name='applicants'),
     path('view-reservations/<int:catering_service_id>/',view_reservations,name='view_reservations'),
     path('view-reservations/<int:catering_service_id>/view_reservation/<int:event_id>/',view_reservation,name='view_reservation'),
+    path('catering-calendar/<int:catering_service_id>/<int:year>/<int:month>', catering_calendar_view, name='catering_calendar'),
+    path('catering-calendar/<int:catering_service_id>/<int:year>/<int:month>/next_month/', next_month_view, name='next_month'),
+    path('catering-calendar/<int:catering_service_id>/<int:year>/<int:month>/prev_month/', prev_month_view, name='prev_month'),
+    path('catering-calendar/<int:catering_service_id>/<int:year>/<int:month>/<int:day>/', reservations_for_day, name='reservations_for_day'),
 ]
--- banquetBuddy/catering_owners/views.py
+++ banquetBuddy/catering_owners/views.py
@@ -4,8 +4,12 @@ from django.contrib import messages
 from django.http import HttpResponse, HttpResponseForbidden
 from core.models import *
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from .forms import EmployeeFilterForm
+from datetime import datetime
+from django.utils.dateformat import DateFormat
+import calendar
+
 
 # Create your views here.
 
@@ -31,12 +35,12 @@ def employee_applications(request, offer_id):
 def view_reservations(request, catering_service_id):
 
     catering_service = get_object_or_404(CateringService, pk=catering_service_id)
-    if request.user == catering_service.cateringcompany.user:  
-        catering_service = get_object_or_404(CateringService, pk=catering_service_id)
-        reservations = catering_service.events.all()
-        return render(request,'reservations.html',{'reservations': reservations,'catering_service':catering_service})
-    else:
-        return HttpResponseForbidden("You don't have permission to view this reservations.")
+    #if request.user == catering_service.cateringcompany.user:  
+    catering_service = get_object_or_404(CateringService, pk=catering_service_id)
+    reservations = catering_service.events.all()
+    return render(request,'reservations.html',{'reservations': reservations,'catering_service':catering_service})
+    #else:
+    #    return HttpResponseForbidden("You don't have permission to view this reservations.")
     
 @login_required
 def view_reservation(request, event_id,catering_service_id):
@@ -46,3 +50,72 @@ def view_reservation(request, event_id,catering_service_id):
         return render(request,'view_reservation.html',{'event': event})
     else:
         return HttpResponseForbidden("You don't have permission to view this reservation.")
+
+
+def catering_calendar_view(request, catering_service_id,month,year):
+    catering_service = get_object_or_404(CateringService, pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:  
+        month = min(max(int(month), 1), 12)  # Asegurarse de que el mes esté en el rango válido (1-12)
+
+        month_name = DateFormat(datetime(year, month, 1)).format('F')
+
+    # Obtener el calendario del mes y los días con eventos
+        cal = calendar.monthcalendar(year, month)
+        events = Event.objects.filter(
+            cateringservice=catering_service,
+            date__year=year,
+            date__month=month
+    ).values_list('date__day', flat=True)
+
+    # Generar una lista de días con eventos para resaltar en el calendario
+        event_days = set(events)
+
+
+        return render(request, 'calendar.html', {
+            'catering_name': catering_service.name,
+            'catering_service_id':catering_service_id,
+            'year': year,
+            'month': month,
+            'month_name':month_name,
+            'cal': cal,
+            'event_days': event_days
+        })
+    else:
+        return HttpResponseForbidden("You don't have permission to view this calendar.")
+
+def reservations_for_day(request, catering_service_id, year, month, day):
+    catering_service = CateringService.objects.get(pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:
+        selected_date = datetime(year, month, day)
+        reservations = Event.objects.filter(cateringservice=catering_service, date=selected_date)
+        return render(request, 'reservations_for_day.html', {'catering_service': catering_service,'selected_date': selected_date, 'reservations': reservations})
+    else:
+        return HttpResponseForbidden("You don't have permission to view this day reservations.")
+    
+
+def next_month_view(request, catering_service_id, year, month):
+    catering_service = CateringService.objects.get(pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:
+        next_month = int(month) + 1
+        next_year = int(year)
+        if next_month > 12:
+            next_month = 1
+            next_year += 1
+    else:
+        return HttpResponseForbidden("You don't have permission to manipulate this calendar.")
+
+    return redirect('catering_calendar', catering_service_id=catering_service_id, year=next_year, month=next_month)
+
+def prev_month_view(request, catering_service_id, year, month):
+    catering_service = CateringService.objects.get(pk=catering_service_id)
+    if request.user == catering_service.cateringcompany.user:
+        prev_month = int(month) - 1
+        prev_year = int(year)
+        if prev_month < 1:
+            prev_month = 12
+            prev_year -= 1
+    else:
+        return HttpResponseForbidden("You don't have permission to manipulate this calendar.")
+    
+    return redirect('catering_calendar', catering_service_id=catering_service_id, year=prev_year, month=prev_month)
+
