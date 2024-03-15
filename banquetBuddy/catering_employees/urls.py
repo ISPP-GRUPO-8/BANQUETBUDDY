54458b33c61b8e9b8c092cc67eea0513e72ba1b4
fravarcas <xxraptor51esp@gmail.com>
2024-03-15 14:29:58 +0100

    feat: Añadida acción de aplicar para ofertas
    Se han añadido las vistas y operaciones necesarias para aplicar a una
    oferta

--- banquetBuddy/catering_employees/urls.py
+++ banquetBuddy/catering_employees/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('register_employee',views.register_employee,name='register_employee'),
     path('applicants/<int:offer_id>/', views.employee_applications, name='applicants'),
+    path('employeeOfferList', views.employee_offer_list, name='employeeOfferList'),
+    path('employeeApplication/<int:offer_id>/', views.application_to_offer, name='application_to_offer')
 ]
