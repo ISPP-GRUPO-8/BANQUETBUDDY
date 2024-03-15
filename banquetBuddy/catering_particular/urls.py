fcf9954f4836571de165e7166542dcc36f9ffd2f
Alvsanflo <100154520+Alvsanflo@users.noreply.github.com>
2024-03-15 13:26:15 +0100

    fix: Caterings contratados
    Arreglado fallo en urls.py

--- banquetBuddy/catering_particular/urls.py
+++ banquetBuddy/catering_particular/urls.py
@@ -5,7 +5,7 @@ from catering_particular import views
 from .views import *
 
 urlpatterns = [
-    path('caterings_contratados', catering_contratados, name='catering_contratados')
+    path('caterings_contratados', catering_contratados, name='catering_contratados'),
     path('', views.listar_caterings, name='listar_caterings'),
     path('<int:catering_id>/', views.catering_detail, name='catering_detail'),
     path('register_particular',views.register_particular,name='register_particular'),
