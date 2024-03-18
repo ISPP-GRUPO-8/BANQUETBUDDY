4b8856ad58d5fd7334723f9a6436ec9241cbc564
gonsanmar2 <gonsanmar2@alum.us.es>
2024-03-18 18:43:24 +0100

    feat: Control de Pagos
    Añadida pasarela de pago al reservar un catering

--- banquetBuddy/catering_particular/urls.py
+++ banquetBuddy/catering_particular/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', views.listar_caterings, name='listar_caterings'),
     path('<int:catering_id>/', views.catering_detail, name='catering_detail'),
     path('register_particular',views.register_particular,name='register_particular'),
+    path('process/', views.payment_process, name='process'),
+    path('completed/', views.payment_completed, name='completed'),
+    path('canceled/', views.payment_canceled, name='canceled'),
 ]
