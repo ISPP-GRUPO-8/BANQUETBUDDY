e7cfa061d9a4be1c6485b27e77db08191a538b86
jaicabher1 <jaicabher1@alum.us.es>
2024-04-08 20:26:55 +0200

    feat: test of method

--- banquetBuddy/catering_particular/tests.py
+++ banquetBuddy/catering_particular/tests.py
@@ -473,3 +473,73 @@ class FiltrosTest(TestCase):
         self.assertEqual(response.status_code, 200)
         # Asegúrate de que no hay filtros de ciudad en la respuesta
         self.assertFalse(response.context["ciudad"])
+        
+        
+class CateringViewTest(TestCase):
+    def setUp(self):
+        # Configurar el entorno de prueba con objetos necesarios
+        self.client = Client()
+
+        self.user = CustomUser.objects.create_user(username='test_user', password='test_password',email='testuser@gmail.com')
+        self.user1 = CustomUser.objects.create_user(username='test_user1', password='test_password1',email='testuser1@gmail.com')
+
+        self.catering_company = CateringCompany.objects.create(user=self.user, name='Test Catering Company', price_plan="PREMIUM_PRO")
+        
+        self.message = Message.objects.create(
+            sender=self.user,
+            receiver=self.user1,
+            date=datetime.now(),
+            content="Este es un mensaje de ejemplo."
+        )
+
+    def test_listar_caterings_particular(self):
+        # Simular una solicitud HTTP al punto final
+        self.client.force_login(self.user)
+
+        # Realizar la solicitud HTTP
+        response = self.client.get(reverse('listar_caterings_particular'))
+
+        # Verificar si la respuesta es exitosa
+        self.assertEqual(response.status_code, 200)
+
+        # Verificar si el template utilizado es el esperado
+        self.assertTemplateUsed(response, 'contact_chat_owner.html')
+        
+        # Verificar si el contexto se pasa correctamente al template
+        self.assertTrue(response.context['is_catering_company'])
+        self.assertIn('messages', response.context)
+
+    def test_listar_caterings_particular_unauthenticated(self):
+        # Realizamos una solicitud GET a la vista sin autenticar al usuario
+        response = self.client.get(reverse('listar_caterings_particular'))
+
+        # Verificamos que el usuario no autenticado reciba un código de estado 302 para redirigirlo 
+        self.assertEqual(response.status_code, 302)
+
+    def test_listar_caterings_companies_unauthenticated(self):
+        # Realizamos una solicitud GET a la vista sin autenticar al usuario
+        response = self.client.get(reverse('listar_caterings_companies'))
+
+        # Verificamos que el usuario no autenticado reciba un HttpResponseForbidden
+        self.assertIsInstance(response, HttpResponseForbidden)
+
+    def test_listar_caterings_companies_authenticated_as_particular(self):
+        # Simulamos una solicitud HTTP autenticada como un usuario particular
+        self.client.force_login(self.user)
+
+        # Realizamos una solicitud GET a la vista
+        response = self.client.get(reverse('listar_caterings_companies'))
+
+        # Verificamos que el usuario particular reciba un HttpResponseForbidden
+        self.assertIsInstance(response, HttpResponseForbidden)
+        
+    def test_listar_caterings_companies_authenticated_as_employee(self):
+        # Simulamos una solicitud HTTP autenticada como un usuario empleado
+        employee = Employee.objects.create(user=self.user1)
+        self.client.force_login(self.user1)
+
+        # Realizamos una solicitud GET a la vista
+        response = self.client.get(reverse('listar_caterings_companies'))
+
+        # Verificamos que el usuario empleado reciba un HttpResponseForbidden
+        self.assertIsInstance(response, HttpResponseForbidden)
