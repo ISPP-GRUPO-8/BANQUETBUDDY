1f69d43c3a289b720fb68c8f2beac41f961d4b7f
juamarcan8 <juanmc015@gmail.com>
2024-04-19 18:51:32 +0200

    test: Booking process

--- banquetBuddy/catering_particular/test_selenium.py
+++ banquetBuddy/catering_particular/test_selenium.py
@@ -28,3 +28,30 @@ class TestLogin():
     self.driver.find_element(By.CSS_SELECTOR, ".form-group:nth-child(3)").click()
     self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
   
+class TestBooking():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_booking(self):
+    self.driver.get("http://127.0.0.1:8000/")
+    self.driver.set_window_size(2576, 1416)
+    self.driver.find_element(By.LINK_TEXT, "Log in").click()
+    self.driver.find_element(By.ID, "id_username").click()
+    self.driver.find_element(By.ID, "id_username").send_keys("Pablo@gmail.com")
+    self.driver.find_element(By.ID, "id_password").click()
+    self.driver.find_element(By.ID, "id_password").send_keys("Pablo")
+    self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    self.driver.find_element(By.LINK_TEXT, "Caterings").click()
+    self.driver.find_element(By.LINK_TEXT, "View details").click()
+    self.driver.find_element(By.LINK_TEXT, "Book").click()
+    self.driver.find_element(By.NAME, "event_date").click()
+    self.driver.find_element(By.NAME, "event_date").send_keys("2026-01-19")
+    self.driver.find_element(By.NAME, "number_guests").click()
+    self.driver.find_element(By.NAME, "number_guests").send_keys("100")
+    self.driver.find_element(By.CSS_SELECTOR, ".col-md-4:nth-child(1) .btn").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+  
