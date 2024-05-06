b116e4a551c67e3a7d269ea1b77320b9af13e73a
ferbarbar1 <ferbarbar1@alum.us.es>
2024-05-06 18:04:42 +0200

    fix: Solved security hostpost

--- banquetBuddy/banquetBuddy/settings.py
+++ banquetBuddy/banquetBuddy/settings.py
@@ -35,7 +35,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "banquetbuddyoficial@gmail.com"
-EMAIL_HOST_PASSWORD = "zsqt bsae cayb atuk"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = "banquetbuddyoficial@gmail.com"
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
