3734bdf8b0a6582fd02e5589ae9ca8ba51df66ba
ferbarbar1 <ferbarbar1@alum.us.es>
2024-05-06 18:28:24 +0200

    fix: Modified settings

--- banquetBuddy/banquetBuddy/settings.py
+++ banquetBuddy/banquetBuddy/settings.py
@@ -44,7 +44,7 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-oe0$c66l3=!r-4uq*c45axvjq90v!-n5e!dtq)+b)*bpil@nf$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
